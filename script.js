b8c5c88d2f88e7660036e66fa813dde9b537020b
marievy <ivymarieporras@gmail.com>
2025-04-15 20:17:47 -0600

    fix: navigation color and position if not main html

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+bodyContainer =  document.querySelector('body');
 
 // Menu toggle
 toggleNav.addEventListener('click', function () {
    toggleNav.classList.toggle('open');
+   mainNav.classList.toggle('active');
    navList.classList.toggle('active');
+   // Only change the logo if the body has the "page" class 
+   if (bodyContainer.classList.contains("page")) {
+      if (mainNav.classList.contains("active")) {
+         logo.src = "logo.svg";
+      } else {
+         logo.src = "logo-light.svg";
+      }
+   }
 });
