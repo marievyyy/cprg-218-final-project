3ed8dc871d6524d97ce0455c28ce94baa32b0262
marievy <ivymarieporras@gmail.com>
2025-04-17 10:18:13 -0600

    add: nav active

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ toggleNav.addEventListener('click', function () {
    toggleNav.classList.toggle('open');
    mainNav.classList.toggle('active');
    navList.classList.toggle('active');
+   bodyContainer.classList.toggle('nav-active');
    // Only change the logo if the body has the "page" class 
    if (bodyContainer.classList.contains("page")) {
       if (mainNav.classList.contains("active")) {
-         logo.src = "logo.svg";
+         logoImg.src = "logo.svg";
       } else {
-         logo.src = "logo-light.svg";
+         logoImg.src = "logo-light.svg";
       }
    }
 });
+
+// Nav active
+const links = document.querySelectorAll('nav a');
+   links.forEach(link => {
+      if (link.href === window.location.href) {
+      link.classList.add('active');
+   }
+});
+
 
 // Weather
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=e361073631e2c1f68dcc5845794ce885&units=metric`)
