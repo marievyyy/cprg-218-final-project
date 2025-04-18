880735e68fe5fea5d13c339f3a5aade1519b4197
marievy <ivymarieporras@gmail.com>
2025-04-18 15:31:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,26 @@ bodyContainer =  document.querySelector('body');
 
 // Menu toggle
 toggleNav.addEventListener('click', function () {
-   toggleNav.classList.toggle('open');
-   mainNav.classList.toggle('active');
-   navList.classList.toggle('active');
-   bodyContainer.classList.toggle('nav-active');
-   // Only change the logo if the body has the "page" class 
-   if (bodyContainer.classList.contains("page")) {
-      if (mainNav.classList.contains("active")) {
-         logoImg.src = "logo.svg";
-      } else {
-         logoImg.src = "logo-light.svg";
-      }
-   }
+  toggleNav.classList.toggle('open');
+  mainNav.classList.toggle('active');
+  navList.classList.toggle('active');
+  bodyContainer.classList.toggle('nav-active');
+  // Only change the logo if the body has the "page" class 
+  if (bodyContainer.classList.contains("page")) {
+    if (mainNav.classList.contains("active")) {
+      logoImg.src = "logo.svg";
+    } else {
+      logoImg.src = "logo-light.svg";
+    }
+  }
 });
 
 // Nav active
 const links = document.querySelectorAll('nav a');
-   links.forEach(link => {
-      if (link.href === window.location.href) {
-      link.classList.add('active');
-   }
+  links.forEach(link => {
+    if (link.href === window.location.href) {
+    link.classList.add('active');
+  }
 });
 
 // animation
@@ -69,9 +69,9 @@ window.addEventListener('scroll', () => {
 
 // Weather
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=e361073631e2c1f68dcc5845794ce885&units=metric`)
-   .then(response => response.json())
-   .then(data => {
-      console.log(data);
-      temp.textContent = `${data.main.temp} \u00B0C`;
-      condition.textContent = `${data.weather[0].description}`;
-   });
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    temp.textContent = `${data.main.temp} \u00B0C`;
+    condition.textContent = `${data.weather[0].description}`;
+});
